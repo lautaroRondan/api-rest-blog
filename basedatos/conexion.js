d2b1848b2b5777abffc33d5ca6f0a85b9c98a50b
lautaro <lautarorondan96@gmail.com>
2022-10-21 15:02:50 -0300

    conexion base de datos

--- basedatos/conexion.js
+++ basedatos/conexion.js
@@ -5,8 +5,8 @@ const conexion = async()=>{
 
     try{
 
-       await mongoose.connect("mongodb://localhost:27017/mi_blog");
-
+    //    await mongoose.connect("mongodb://localhost:27017/mi_blog");
+       await mongoose.connect("mongodb+srv://lauti:lutyluty8@api-rest-blog.s47xrmn.mongodb.net/mi_blog");
        console.log("conectado correctamente");
 
     }catch(error){
